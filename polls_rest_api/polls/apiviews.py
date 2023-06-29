a3e2b64b4d1189fd5e6eae44262b4c85e45948b0
ashrafkvt <ashrafkvt6197@gmail.com>
2023-06-29 11:32:45 +0530

    implement view for creating votes

--- polls_rest_api/polls/apiviews.py
+++ polls_rest_api/polls/apiviews.py
@@ -1,6 +1,6 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework import generics
+from rest_framework import generics, status
 from django.shortcuts import get_object_or_404
 
 from .models import Poll, Choice
@@ -28,6 +28,14 @@ class ChoiceList(generics.ListCreateAPIView):
         return queryset
     
 
-class CreateVote(generics.CreateAPIView):
+class CreateVote(APIView):
 
-    serializer_class = VoteSerializer
+    def post(self, request, pk, choice_pk):
+        voted_by = request.data.get("voted_by")
+        data = {'choice': choice_pk, 'poll': pk, 'voted_by': voted_by}
+        serializer = VoteSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
--- polls_rest_api/polls/urls.py
+++ polls_rest_api/polls/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path("polls/", PollList.as_view(), name="polls_list"),
     path("polls/<int:poll_id>/", PollDetail.as_view(), name="polls_detail"),
     path("polls/<int:pk>/choices", ChoiceList.as_view(), name="choice_list"),
-    path("polls/vote", CreateVote.as_view(), name="create_vote"),
+    path("polls/<int:pk>/choices/<int:choice_pk>/vote/", CreateVote.as_view(), name="create_vote"),
 ]
