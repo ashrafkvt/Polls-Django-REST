a48c5b0583ddb4c99ead96c955b28ce26273a651
ashrafkvt <ashrafkvt6197@gmail.com>
2023-07-08 20:47:41 +0530

    add DRF authentication schemes

--- polls_rest_api/polls_rest_api/settings.py
+++ polls_rest_api/polls_rest_api/settings.py
@@ -133,3 +133,10 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 SHELL_PLUS = "ipython"
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+    )
+}
