e6c49b4f9b62780f6a09738972af36bc9004cb37
ashrafkvt <ashrafkvt6197@gmail.com>
2023-07-09 17:25:49 +0530

    write login view API

--- polls_rest_api/polls/apiviews.py
+++ polls_rest_api/polls/apiviews.py
@@ -2,13 +2,15 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import generics, status
 from django.shortcuts import get_object_or_404
+from django.contrib.auth import authenticate
+from django.http import request
 
 from .models import Poll, Choice
 from  .serializers import PollSerializer, ChoiceSerializer, \
     VoteSerializer, UserSerializer
 
 class PollList(generics.ListCreateAPIView):
-
+    
     queryset = Poll.objects.all()
     serializer_class = PollSerializer
 
@@ -44,3 +46,15 @@ class UserCreate(generics.CreateAPIView):
     authentication_classes = ()
     permission_classes = ()
     serializer_class = UserSerializer
+
+class LoginView(APIView):
+    permission_classes = ()
+
+    def post(self, request,):
+        username = request.data.get('username')
+        password = request.data.get('password')
+        user = authenticate(username=username, password=password)
+        if user:
+            return Response({"token": user.auth_token.key})
+        else:
+            return Response({"error": "Wrong Credentials"}, status=status.HTTP_400_BAD_REQUEST)
--- polls_rest_api/polls/urls.py
+++ polls_rest_api/polls/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .apiviews import PollList, PollDetail,\
-      ChoiceList, CreateVote, UserCreate
+from .apiviews import PollList, PollDetail, \
+      ChoiceList, CreateVote, UserCreate, \
+      LoginView
 
 urlpatterns = [
     path("polls/", PollList.as_view(), name="polls_list"),
@@ -8,4 +9,5 @@ urlpatterns = [
     path("polls/<int:pk>/choices", ChoiceList.as_view(), name="choice_list"),
     path("polls/<int:pk>/choices/<int:choice_pk>/vote/", CreateVote.as_view(), name="create_vote"),
     path("users/", UserCreate.as_view(), name="user_create"),
+    path("login/", LoginView.as_view(), name="login_view"),
 ]
